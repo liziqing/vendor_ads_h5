45662d87c727bee2ee9f6676fa8e6f33e21155b8
nxy <xiaoyang91315@sina.com>
2016-09-20 17:41:34 +0800

    up

--- src/pt_emoticon/js/wechat.js
+++ src/pt_emoticon/js/wechat.js
@@ -62,7 +62,7 @@ $(function(){
 
 
     var video = new WxMoment.Video({
-        vid: "i00184ka7lw",
+        vid: "p0021ehy1js",
         pic: "img/wechatfeeds/cover.png",
         oninited: function () {
             //console.log(0)
@@ -92,17 +92,46 @@ $(function(){
     });
 
 
-    //
-    //ptShareConfig.init(function (data) {
-    //    wx.config({
-    //        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    //        appId: '', // 必填，公众号的唯一标识
-    //        timestamp: data.timestamp, // 必填，生成签名的时间戳
-    //        nonceStr: data.noncestr, // 必填，生成签名的随机串
-    //        signature: data.signature, // 必填，签名，见附录1
-    //        jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-    //    });
-    //});
 
+    ptShareConfig.init(function (data) {
+        wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: 'wx9a8bd58d910b0460', // 必填，公众号的唯一标识
+            timestamp: data.timestamp, // 必填，生成签名的时间戳
+            nonceStr: data.noncestr, // 必填，生成签名的随机串
+            signature: data.signature, // 必填，签名，见附录1
+            jsApiList: [
+                'onMenuShareTimeline', 'onMenuShareAppMessage'
+            ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+    });
+
+
+    wx.onMenuShareTimeline({
+        title: '测试测试测试测试测试',// 分享标题
+        link: window.location.href, // 分享链接
+        imgUrl: 'http://' + window.location.origin + '/img/wechatfeeds/1.png', // 分享图标
+        success: function () {
+            // 用户确认分享后执行的回调函数
+        },
+        cancel: function () {
+            // 用户取消分享后执行的回调函数
+        }
+    });
+
+    wx.onMenuShareAppMessage({
+        title: '测试测试测试测试测试', // 分享标题
+        link: window.location.href, // 分享链接
+        desc: '测试测试测试测试测试',
+        imgUrl: 'http://' + window.location.origin + '/img/wechatfeeds/1.png',
+        type: '', // 分享类型,music、video或link，不填默认为link
+        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+        success: function () {
+            // 用户确认分享后执行的回调函数
+        },
+        cancel: function () {
+            // 用户取消分享后执行的回调函数
+        }
+    });
 
 });
