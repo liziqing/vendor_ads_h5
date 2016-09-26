7e00970a848b3f7c3aafb45f6973f0fca489bcdc
ianzhang001 <24033474@qq.com>
2016-09-26 16:24:45 +0800

    up

--- src/pt_emoticon/pc/js/main.js
+++ src/pt_emoticon/pc/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     resizeIframe();
     $(window).resize(resizeIframe);
+
+    $("#download").click(function(e){
+        _smq.push(['custom', '监测代码', '点击下载电脑版QQ表情包']);
+    })
 })
 
 function resizeIframe(){
