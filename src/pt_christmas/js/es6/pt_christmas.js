e36f85b7d6c015bb8ee4548cd62b2df97e5d2355
martinni <martin.ni@yiyatech.com>
2016-12-05 16:41:36 +0800

    up

--- src/pt_christmas/js/es6/pt_christmas.js
+++ src/pt_christmas/js/es6/pt_christmas.js
@@ -1,14 +1,14 @@
 /**
  * Created by martin on 16/11/21.
  */
-define(['wx','base/env', 'base/wx', 'base/util','jquery', 'hammer', 'velocity'], function(wx,env,baseWx,util,$, Hammer, Velocity) {
-        $(() => {
+define(['wx','base/env', 'base/wx', 'base/util','jquery', 'hammer', 'velocity', 'ptShareConfig'],
+    function(wx,env,baseWx,util,$, Hammer, Velocity, ptShareConfig) {
 
+        $(() => {
 
             let head;
             let nickname;
             let openid;
-
 
             let myDate=new Date();
             let hour=myDate.getHours();
@@ -17,7 +17,6 @@ define(['wx','base/env', 'base/wx', 'base/util','jquery', 'hammer', 'velocity'],
             let day=myDate.getDate();
             let week=myDate.getDay();
             let weekArr=['日','一','二','三','四','五','六'];
-
 
             let hot = document.getElementById('hot_area');
             let swipe = document.getElementById('swipe_hint');
@@ -29,8 +28,6 @@ define(['wx','base/env', 'base/wx', 'base/util','jquery', 'hammer', 'velocity'],
             let deltaY = 0;
             let swiperDone = false;
 
-
-
             let timer;
             let animateAble = true;
             let chatIndex = 0;
@@ -38,15 +35,62 @@ define(['wx','base/env', 'base/wx', 'base/util','jquery', 'hammer', 'velocity'],
 
             let endLock = false;
 
+            let shareData = {
+                shareUrl: 'shareUrl',
+                shareImg: 'shareImg',
+                shareFeedsTile: 'shareFeedsTile',
+                shareTile: 'shareTile',
+                shareDes: 'shareDes'
+            };
+
             pullUserInfo();
+            // wxInit();
 
+            function wxInit() {
+                ptShareConfig.init(function (data) {
+                    wx.config({
+                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: 'wx9a8bd58d910b0460', // 必填，公众号的唯一标识
+                        timestamp: data.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: data.noncestr, // 必填，生成签名的随机串
+                        signature: data.signature, // 必填，签名，见附录1
+                        jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    });
 
+                    wx.ready(function () {
+                        wx.onMenuShareTimeline({
+                            title: shareData.shareFeedsTile, // 分享标题
+                            link: shareData.shareUrl, // 分享链接
+                            imgUrl: shareData.shareImg,
+                            success: function success() {
+                                // 用户确认分享后执行的回调函数
+                            },
+                            cancel: function cancel() {
+                                // 用户取消分享后执行的回调函数
+                            }
+                        });
+
+                        wx.onMenuShareAppMessage({
+                            title: shareData.shareTile, // 分享标题
+                            link: shareData.shareUrl,
+                            desc: shareData.shareDes,
+                            imgUrl: shareData.shareImg,
+                            type: '', // 分享类型,music、video或link，不填默认为link
+                            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                            success: function success() {
+                                // 用户确认分享后执行的回调函数
+                            },
+                            cancel: function cancel() {
+                                // 用户取消分享后执行的回调函数
+                            }
+                        });
+                    });
+                });
+            }
 
             /**
              第一页
              */
-
-
 
             /**
              滑动解锁页时间显示
@@ -88,7 +132,7 @@ define(['wx','base/env', 'base/wx', 'base/util','jquery', 'hammer', 'velocity'],
             function  pullUserInfo(){
                 if(env.debug == 1)
                 {
-                    //head = './img/xiena-head.jpg';//  ？缺少默认头像
+                    head = './img/user-head-null.png';
 
                     nickname = 'test';
 
