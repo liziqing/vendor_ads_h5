b14003716058a82dc14eab5ee3c3a28771926456
martinni <martin.ni@yiyatech.com>
2016-12-06 18:37:50 +0800

    up

--- dist/pt_christmas/js/pt_christmas.js
+++ dist/pt_christmas/js/pt_christmas.js
@@ -3,7 +3,7 @@
 /**
  * Created by martin on 16/11/21.
  */
-define(['wx', 'base/env', 'base/wx', 'base/util', 'jquery', 'hammer', 'velocity', 'ptShareConfig'], function (wx, env, baseWx, util, $, Hammer, Velocity, ptShareConfig) {
+define(['wx', 'base/env', 'base/wx', 'base/util', 'jquery', 'hammer'], function (wx, env, baseWx, util, $, Hammer) {
 
     $(function () {
 
