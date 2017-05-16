4878b1a768965d23eba9573d87d060fb47e8c9cc
sjs <jisheng.sun@yiyatech.com>
2017-05-16 13:34:18 +0800

    edit

--- src/ksf_greentea/js/index.js
+++ src/ksf_greentea/js/index.js
@@ -269,7 +269,7 @@ define(['wx', 'base/env', 'base/wx', 'base/util', 'jquery', 'swiper', 'imgLoadCa
             shareTitle: '【康师傅绿茶健康活力派】',
             shareUrl: 'http://kangshifu.qnmami.com',
             shareImg: 'http://kangshifu.qnmami.com/img/slogan.png',
-            shareDes: '可以见峰峰和吴磊咯！赶紧一起京东下单一箱康师傅绿茶，有机会得到李易峰x吴磊见面会门票！'
+            shareDes: '快一起来参加【#康师傅绿茶健康活力派#活力时刻】，赢李易峰x吴磊见面会门票~'
         };
 
         baseWx.initWxJs('wx8e56a8ebb0688ab9', 'pt_christmas', ['onMenuShareTimeline', 'onMenuShareAppMessage'], function () {
